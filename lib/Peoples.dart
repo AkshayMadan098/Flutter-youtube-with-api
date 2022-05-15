f724410d257e8ab4aab427070b191cdc4ca15d36
Akshay Madan <40893683+silentkiller12345@users.noreply.github.com>
2022-05-15 23:48:40 +0530

    user list added

---
+++ lib/Peoples.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class People extends StatefulWidget {
+  @override
+  _PeopleState createState() => _PeopleState();
+}
+
+class _PeopleState extends State<People> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
